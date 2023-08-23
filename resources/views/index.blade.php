f00d2fac0acc70e9a618b99f3c58f5a69c0bea6a
suziemweshn <suziemweshn@gmail.com>
2023-08-23 17:20:18 +0300

    committed the changes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="{{asset('style.css')}}">
+</head>
+<body>
+    <h1>Hello world</h1>
+    <div class="container d-flex flex-row">
+        <div class="row ">
+            <div class="col-lg-4 col-md-6 col-sm-12  ">
+                <div class="card fade-card" style="margin-bottom:10px">
+                    <div class="card-header">
+                        <p>card 1</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-img-top">
+                            <img src="{{asset('astroemelia.jpg')}}" alt="" height="200px" width="300px">
+
+                        </div>
+                        <div class="card-footer">
+                        <button type="button" class="btn-btn-danger w-100 " ><a href=""></a>submit</button>
+                        </div>
+                    </div>
+                </div>
+</div>
+
+         
+       
+        <div class="col-lg-4 col-md-6 col-sm-12 ">
+                <div class="card fade-card">
+                    <div class="card-header">
+                        <p>card 1</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-img-top ">
+                            <img src="{{asset('astroemelia.jpg')}}" alt="" height="200px" width="300px">
+
+                        </div>
+                        <div class="card-footer">
+                        <button class="btn-btn-danger w-100 " ><a href=""></a>submit</button>
+                        </div>
+                    </div>
+                </div>
+
+           
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-6">
+                <div class="card fade-card">
+                    <div class="card-header">
+                        <p>card 1</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-img-top">
+                            <img src="{{asset('astroemelia.jpg')}}" alt="" height="200px" width="300px">
+
+                        </div>
+                        <div class="card-footer">
+                        <button class="btn-btn-danger w-100 "><a href=""></a>submit</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+       
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-6">
+                <div class="card fade-card">
+                    <div class="card-header">
+                        <p>card 1</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-img-top">
+                            <img src="{{asset('astroemelia.jpg')}}" alt="" height="200px" width="300px">
+
+                        </div>
+                        <div class="card-footer">
+                        <button class="btn-btn-danger w-100 " ><a href=""></a>submit</button>
+                        </div>
+                    </div>
+                </div>
+
+</div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-5">
+                <div class="card fade-card">
+                    <div class="card-header">
+                        <p>card 1</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-img-top">
+                            <img src="{{asset('astroemelia.jpg')}}" alt="" height="200px" width="300px">
+
+                        </div>
+                        <div class="card-footer">
+                        <button class="btn-btn-danger w-100 " ><a href=""></a>submit</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+</div>
+</div>
+  
+</body>
+</html>
